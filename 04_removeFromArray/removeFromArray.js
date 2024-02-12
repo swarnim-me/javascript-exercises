64af5b09659fda41122905f2841749ceacdfa748
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-12 23:33:21 +0530

    Bruteforce exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, value1, value2, value3, value4) {
+    let args = [value1, value2, value3, value4];
+    for (const arg of args) {
+        if (arg) {
+            for (let i = 0; i < array.length; i++) {
+                while (array[i] === arg) {
+                    array.splice(i, 1);
+                }
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
