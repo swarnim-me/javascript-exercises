dc2bbdf543787ec481ce3a8097c66d012ebebe43
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-12 22:34:11 +0530

    Complete exercise 2 and remove skip from test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (stringToRepeat, numberOfTimes) {
+    if (numberOfTimes < 0) return "ERROR";
+    let output = "";
+    for (let i = 0; i < numberOfTimes; i++) {
+        output += stringToRepeat;
+    }
+    return output;
 };
 
 // Do not edit below this line
