de41ae6199057ce8627df798d1b33e544f4cda8c
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-12 22:37:42 +0530

    Complete exercise 3 and remove skip from test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+    let output = "";
+    for (let i = stringToReverse.length - 1; i >= 0; i--) {
+        output += stringToReverse[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
