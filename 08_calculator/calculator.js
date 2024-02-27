02f0b9fc6da3a0ea9010df6be109b6db1f0607c8
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-27 19:54:47 +0530

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, currentValue) => total + currentValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, currentValue) => total * currentValue, 1)
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  return factorial(num - 1) * num;
 };
 
 // Do not edit below this line
