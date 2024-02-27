10b7a4f30f3d4e976939ea8f5ccc7b31ce82c165
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-27 20:20:21 +0530

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    return fibonacci(num - 2) + fibonacci(num - 1);
 };
 
 // Do not edit below this line
