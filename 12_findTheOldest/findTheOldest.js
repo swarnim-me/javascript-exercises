2861f63748bd9d8ec1ceb235f801bcd6e2286065
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-27 21:57:19 +0530

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let maxAge = -1;
+    const currentYear = new Date().getFullYear();
+    let oldest = people[0];
+    people.forEach((person) => {
+        if (!person.yearOfDeath) person.yearOfDeath = currentYear;
+        let age = person.yearOfDeath - person.yearOfBirth;
+        console.log(age);
+        if (age > maxAge) {
+            maxAge = age;
+            oldest = person;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
